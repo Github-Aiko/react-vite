e77d168050f768e2b79d7407bb090efef9dd7101
Aiko <aikocute@icloud.com>
2024-07-19 23:56:56 +0700

    update

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,9 +1,9 @@
-import { Link, NavLink } from 'react-router-dom';
-import './header.css'
+import { NavLink } from 'react-router-dom';
+import './header.css';
 const Header = () => {
     return (
         <ul>
-            <li><NavLink className="active" to="/">Home</NavLink></li>
+            <li><NavLink to="/">Home</NavLink></li>
             <li><NavLink to="/users">Users</NavLink></li>
             <li><NavLink to="/products">Products</NavLink></li>
         </ul>
